56d6c9fce9bcf6f9fde018494b242f9853a48c19
skywokerr <atolwamichael7@gmail.com>
2025-11-11 13:06:10 +0300

    next.config,ts update

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -6,3 +6,22 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
+};
+
+export default withPWA({
+  ...nextConfig
+})
