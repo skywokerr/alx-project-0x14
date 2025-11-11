a46bee1533c2cee23fe5e714dbe8fde03b0452c7
skywokerr <atolwamichael7@gmail.com>
2025-11-11 13:19:48 +0300

    pages update

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#171D22" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+        <meta name="msapplication-TileImage" content="/icons/ms-icon-310x310.png" />
+        <meta name="msapplication-TileColor" content="#171D22" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
